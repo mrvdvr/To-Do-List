d1486c516536a37e44d3ed7131fe7a288364e753
mrvdvr <merveokur1987@gmail.com>
2019-09-18 14:48:14 +0300

    crejs

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname +  "/date.js");
+
 
 const app = express();
 
+const items = ["Watch Udemy", "Study English", "Study Dutch"];
+const workItems = [];
+
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
-  var today = new Date();
+ 
+ let day = date.getDate();
 
-  var options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long"
-  };
+  res.render("list", {listTitle: day, newListItems: items});
+});
 
-  var day = today.toLocaleDateString("en-US", options);
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
 
-  res.render("list", { 
-      kindOfDay: day 
-    });
+  if (req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
 });
+
+app.get("/work", function(req, res) {
+ 
+  res.render("list", {listTitle: "Work list ", newListItems: workItems});
+});
+
+app.get("/about", function (req, res){
+  res.render("about");
+});
+
+
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
